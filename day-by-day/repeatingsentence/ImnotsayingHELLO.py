2f54a0c0247d76aba1b95ed22876c781d98f37ac
MoonJH-2 <eetd0000@gmail.com>
2024-11-30 23:46:07 +0900

    update daybyday

---
+++ day-by-day/repeatingsentence/ImnotsayingHELLO.py
@@ -0,0 +1,25 @@
+# t = int(input())
+# for i in range(t):
+#     a, b = map(int, input().split())
+#     print(a+b)
+
+# n = int(input())
+# r = sum(range(1, n+1))
+# print(r)
+
+# X = int(input())
+# N = int(input())
+# total = 0
+# for i in range(N):
+#     a, b = map(int,input().split())
+#     total += a*b
+# if total == X:
+#     print("Yes")
+# else:
+#     print("No")
+
+# long 4byte, int 4byte
+# N = int(input())
+# c = N//4
+# r = "long " * c + "int"
+# print(r)
